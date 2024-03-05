9c5a1efd48dc950d9aa028da050184aae4680a91
ac-99 <anubhav.chowdhury99@gmail.com>
2024-03-05 21:04:09 +0000

    yum install git lfs

--- train_gemma_2b.sh
+++ train_gemma_2b.sh
@@ -8,6 +8,7 @@
 python3 -m venv gemma-2b-fine-tune
 source gemma-2b-fine-tune/bin/activate
 
+
 # Install specific version of Torch
 pip3 install torch==2.1.2
 
@@ -20,8 +21,22 @@ python -m pip install flash-attn==2.3.6 --no-build-isolation
 # Login to the Hugging Face Hub
 python -c "from huggingface_hub import login; login()"
 
-# Install git-lfs for managing large files
-yum install git-lfs
+
+yum install -y amazon-linux-extras
+
+# Install epel-release from Extras Library
+echo "Installing epel-release..."
+amazon-linux-extras install epel -y 
+
+# Enable epel repository
+echo "Enabling epel repository..."
+yum-config-manager --enable epel
+
+# Install git-lfs
+echo "Installing git-lfs..."
+yum install git-lfs -y
+
+echo "git-lfs installation completed."
 
 # Install Weights & Biases library
 pip install wandb
